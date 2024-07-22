0d67397d03c31556912bcd448be762f27a518425
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-07-22 21:00:53 +0530

    Arrow function

---
+++ 22 07/armstrong/armstrong3.js
@@ -0,0 +1,36 @@
+const count = (n) => {
+    let c = 0;
+    while(n>0){
+        c++;
+        n = parseInt(n/10);
+    }
+    return c;
+}
+const power = (a , c) => {
+    let p = 1;
+    for(let i=1 ; i<=c ; i++){
+        p = p * a;
+    }
+    return p;
+}
+
+const armstrong = (n) => {
+    let s = 0;
+    let c = count(n);
+    while(n > 0){
+        a = n % 10;
+        p = power(a , c);
+        s = s+p;
+        n = parseInt(n / 10);
+    }
+    return s;
+}
+
+let n = 153;
+console.log("Number is " , n);
+let r = armstrong(n);
+if(n==r){
+    console.log("It is a armstrong number");
+}else{
+    console.log("It is not a armstrong number");
+}
