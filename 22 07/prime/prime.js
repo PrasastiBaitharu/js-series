03406c3e86eca5ea1b59169e56f6cf41b1896a7c
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-07-26 21:17:50 +0530

    Arrow Function

---
+++ 22 07/prime/prime.js
@@ -0,0 +1,16 @@
+const prime = () => {
+    let n= 5;
+    console.log("Number is " , n);
+    let c = 0;
+    for(let i=1 ; i<=n ; i++){
+        if(n % i == 0){
+            c++;
+        }
+    }
+    if(c == 2){
+        console.log("It is a prime number");
+    }else{
+        console.log("It is a composite number");
+    }
+}
+prime();
