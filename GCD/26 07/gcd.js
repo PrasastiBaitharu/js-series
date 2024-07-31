36737c10ef12563ac96300ee0410ac98998e6022
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-07-31 22:06:21 +0530

    GCD/HCF of 2 numbers

---
+++ GCD/26 07/gcd.js
@@ -0,0 +1,17 @@
+const gcd = () => {
+    let a = 10;
+    let b = 15;
+    let c = 0;
+    if(a<b){
+        c = a;
+    }else{
+        c = b;
+    }
+    for(let i=1 ; i<=c ; i++){
+        if(a % i == 0 && b % i == 0){
+            ans = i;
+        }
+    }
+    console.log("Gcd of" , a , "and" , b , "is" , ans);  
+}
+gcd();
