bee5becca67c46131dc8f2caf89fb7a19d48b917
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-09-14 23:45:22 +0530

    Area of the rectangle

---
+++ 13 09/rectangle.js
@@ -0,0 +1,6 @@
+let l = 50;
+let b = 30;
+let a = l * b;
+console.log("Length of the rectangle is:" , l);
+console.log("Width of the rectangle is:" , b);
+console.log("Area of the rectangle is:" , a);
