d13b74877169d564322247a6a8a93017c5cc3ebb
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-09-16 23:02:46 +0530

    Digits of a number

---
+++ 13 09/digit.js
@@ -0,0 +1,10 @@
+let n = 153;
+console.log("Number is " , n);
+let a = n % 10;
+let m = parseInt(n/10);
+let b = m % 10;
+let c = parseInt(m / 10);
+console.log("Last digit is" , a);
+console.log("Second digit is" , b);
+console.log("First digit is" , c);
+console.log("Sum of digit is " , a+b+c);
