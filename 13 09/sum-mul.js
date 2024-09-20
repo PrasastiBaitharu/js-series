adf935b8034d520960617d42cfeefeaefef6b26f
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-09-20 23:18:44 +0530

    sum of a and b and multiple of b and c

---
+++ 13 09/sum-mul.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 20;
+let c = 30;
+let s = a + b;
+let m = b * c;
+console.log("Number1 = ", a);
+console.log("Number2 = ", b);
+console.log("Number3 = ", c);
+console.log("Number1 + Number2 = ", s);
+console.log("Number2 * Number3 = ", m);
