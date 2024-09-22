334839c303d42565489ef99fe13f5db1457fd175
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-09-22 23:27:18 +0530

    Check biggest number

---
+++ Conditional statement/big.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 10;
+console.log("First value =" , a);
+console.log("Second value =" , b);
+if(a>b){
+    console.log(a , "is the biggest");
+}else if(a==b){
+    console.log("Both the value is same");
+}else{
+    console.log(b, "is the biggest");
+}
