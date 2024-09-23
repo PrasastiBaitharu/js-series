fc1bb5ebb189fb6a97f2daaa41e699eb6385e203
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-09-23 22:23:50 +0530

    Corespoding day of the week

---
+++ Conditional statement/day.js
@@ -0,0 +1,19 @@
+let n = 4;
+console.log("Day :" , n);
+if(n==1){
+    console.log("Monday");
+}else if(n==2){
+    console.log("Tuesday")
+}else if(n==3){
+    console.log("Wednesday")
+}else if(n==4){
+    console.log("Thrusday")
+}else if(n==5){
+    console.log("Friday")
+}else if(n==6){
+    console.log("Saturday")
+}else if(n==7){
+    console.log("Sunday")
+}else{
+    console.log("Invalid corresponding day");
+}
