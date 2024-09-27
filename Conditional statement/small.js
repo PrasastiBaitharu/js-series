100019143294e91d978c9a5b3be83f718f64c292
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-09-27 22:45:43 +0530

    Print the smallest number

---
+++ Conditional statement/small.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 20;
+let c = 30;
+console.log("First value =" , a);
+console.log("Second value =" , b);
+console.log("Third value =" , c);
+if(a<b && a<c){
+    console.log(a,"is the smallest");
+}else if(b<a && b<c){
+    console.log(b, "is the smallest");
+}else{
+    console.log(c , "is the smallest");
+}
