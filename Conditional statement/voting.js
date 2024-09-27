5a5598c90bd526c200613783198f212a4fed8916
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-09-27 22:42:33 +0530

    Eligible to vote or note

---
+++ Conditional statement/voting.js
@@ -0,0 +1,7 @@
+let a = 18;
+console.log("Age of the person is:" , a);
+if(a>=18){
+    console.log("He/She will be eligible to vote");
+}else{
+    console.log("He/She will not be eligible to vote");
+}
