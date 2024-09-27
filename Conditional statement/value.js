0728e84868c7d91e3630567db6f985c898ab8052
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-09-27 22:44:58 +0530

    Print the biggest number

---
+++ Conditional statement/value.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 20;
+console.log("First value is " , a);
+console.log("Second value is " , b);
+if(a<b){
+    console.log(a , "is smaller than" , b);
+}else{
+    console.log(b , "is smaller than" , a);
+}
