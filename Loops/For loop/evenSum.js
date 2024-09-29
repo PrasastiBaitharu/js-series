ba904c832660b1c2728a15e96f094283175cfab1
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-09-29 23:02:41 +0530

    Sum of even numbers between 1 to 10

---
+++ Loops/For loop/evenSum.js
@@ -0,0 +1,9 @@
+let s = 0;
+console.log("Even numbers from 1 to 10 are:")
+for(let i=1 ; i<=10 ; i++){
+    if(i%2==0){
+        console.log(i);
+        s = s + i;
+    }
+}
+console.log("Sum of even numbers =" , s);
