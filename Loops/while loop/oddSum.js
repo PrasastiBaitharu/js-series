c324a15faf3d0b6b797f441008a11d3805823e76
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-10-07 23:37:55 +0530

    Sum of odd numbers from 1 to 10

---
+++ Loops/while loop/oddSum.js
@@ -0,0 +1,11 @@
+console.log("Odd numbers from 1 to 10 are:");
+let s = 0;
+let i = 1;
+while(i<=10){
+    if(i%2!=0){
+        console.log(i);
+        s=s+i;
+    }
+    i++;
+}
+console.log("Sum of odd numbers are:" , s);
