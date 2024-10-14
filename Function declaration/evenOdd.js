14644627667e8cef3f804b1ec85f185417e0e2f6
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-10-14 23:34:51 +0530

    Check the number is even or odd

---
+++ Function declaration/evenOdd.js
@@ -0,0 +1,10 @@
+function evenOdd(){
+    let n = 6;
+    console.log("Number is:" , n);
+    if(n%2==0){
+        console.log("It is even");
+    }else{
+        console.log("It is odd");
+    }
+}
+evenOdd();
