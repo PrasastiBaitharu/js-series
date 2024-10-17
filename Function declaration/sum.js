ac9b40937ab9f12290b43dfdcfda67b597943263
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-10-17 22:43:34 +0530

    Addition of 2 numbers

---
+++ Function declaration/sum.js
@@ -0,0 +1,9 @@
+function addition(){
+    let a = 10;
+    let b = 20;
+    let s = a + b;
+    console.log("First no.:" , a);
+    console.log("Second no.:" , b);
+    console.log("Sum:" , s);
+}
+addition();
