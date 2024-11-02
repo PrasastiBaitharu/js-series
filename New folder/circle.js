ecb85d63aefc6279314baa4a7f66653ea4a67669
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-11-02 23:07:33 +0530

    Area of the circle

---
+++ New folder/circle.js
@@ -0,0 +1,9 @@
+const circle = (r) => {
+    let pi = 3.14;
+    let area = pi * r * r;
+    return area;
+}
+let r = 10;
+console.log("Radius of the circle:" , r);
+let a = circle(r);
+console.log("Area of the circle:" , a);
