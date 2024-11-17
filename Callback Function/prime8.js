2e55db323ca2afbd527e0d5475ec3a2e1f011fef
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-11-17 22:38:24 +0530

    Check the number is prime or not using callback function

---
+++ Callback Function/prime8.js
@@ -0,0 +1,20 @@
+function showPrimeOrNot(c){
+    if(c==2){
+        console.log("It is a prime number");
+    }else{
+        console.log("It is a composite number");
+    }
+}
+function prime(n, callback2){
+    let c=0;
+    for(let i=1 ; i<=n; i++){
+        if(n%i==0){
+            c++;
+        }
+    }
+    return c
+}
+function checkPrime(callback1, n, callback2){
+    callback2(callback1(n) );
+}
+checkPrime(prime,5, showPrimeOrNot);
