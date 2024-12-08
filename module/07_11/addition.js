6cd8f61af38629af06e2008420929add9ca38abf
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-12-08 22:40:17 +0530

    Modules

---
+++ module/07_11/addition.js
@@ -0,0 +1,3 @@
+const arith = require('./operation.js')
+arith.add(10,20);
+arith.minus(10,20);
---
+++ module/07_11/operation.js
@@ -0,0 +1,9 @@
+function add (a,b){
+    let s = a+b;
+    console.log("Sum is:" , s);
+}
+function minus(a,b){
+    let d = a-b;
+    console.log("Difference is:" , d);
+}
+module.exports={add,minus};
