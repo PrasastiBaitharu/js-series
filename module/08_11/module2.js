41dca8073e4d68ee3bcf00cb76a0e51d62d37834
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-12-10 22:26:25 +0530

    Module

---
+++ module/08_11/module2.js
@@ -0,0 +1,7 @@
+const arith = require('./operation2.js');
+const display = require('./display2.js')
+// arith.display(arith.prime)
+// arith.armstrong(153);
+// arith.strong(145);
+
+display(arith.prime)
