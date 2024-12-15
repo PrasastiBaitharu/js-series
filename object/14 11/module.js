51928f21237bee4bb41ac0a5612bd40ea0b1fc3b
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-12-16 00:12:26 +0530

    Object destructuring

---
+++ object/14 11/module.js
@@ -0,0 +1,3 @@
+const {add , subtract} = require("./module.js");
+add(10,20);
+subtract(10,20);
---
+++ object/14 11/operation.js
@@ -0,0 +1,7 @@
+function add (a,b){
+    console.log("Sum is :" , a+b)
+}
+function subtract (a,b){
+    console.log("Difference is :" , a-b)
+}
+module.exports = {add,subtract};
