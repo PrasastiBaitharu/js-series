fbc4ff287a957aa5b922a7d70bfde3e88c1cf603
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-12-15 21:35:54 +0530

    Assigning data in the object

---
+++ object/12 11/object2.js
@@ -0,0 +1,9 @@
+const person = {
+    "firstName" : "Prasasti",
+    "lastName" : "Baitharu",
+    "age" : 20
+};
+let key = "gender";
+let value = "male";
+person[key] = value;
+console.log(person);
