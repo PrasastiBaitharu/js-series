3d5c31580bf4aa99c237524d9a924e59bac42c97
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2024-12-16 00:13:01 +0530

    Object destructuring

---
+++ object/14 11/object_destructuring.js
@@ -0,0 +1,25 @@
+const myopr1 ={
+    add : function(a,b){
+        console.log("Sum is :" , a+b);
+    },
+    subtract : function(a,b){
+        console.log("Difference is :" , a-b);
+    }
+};
+const myopr2 ={
+    mul : function(a,b){
+        return a*b;
+    },
+    div : function(a,b){
+        return a/b;
+    }
+};
+const myarith = {...myopr1,...myopr2};
+
+const{add,subtract,mul,div}=myarith;//Object destructuring
+
+console.log(myarith);
+add(10,20);
+subtract(10,20);
+console.log(mul(10,20));
+console.log(div(20,10));
