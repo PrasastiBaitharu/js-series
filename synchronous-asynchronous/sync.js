1438cb111993df9d60b29edb194bb8ca46cd0558
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-02-25 21:43:42 +0530

    Synchronous and Asynchronous function in js

---
+++ synchronous-asynchronous/sync.js
@@ -0,0 +1,23 @@
+//Synchronous
+
+function register(){
+    console.log("Register end");
+}
+function sendMail(){
+    console.log("Send mail end");
+}
+function login(){
+    console.log("Login end");
+}
+function getData(){
+    console.log("Get data end");
+}
+function displayData(){
+    console.log("Display data end");
+}
+register();
+sendMail();
+login();
+getData();
+displayData();
+console.log("Other application executed")
